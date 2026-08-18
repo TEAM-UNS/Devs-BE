d8cbf9607791090ae1e894fccf6e1b3edbdfb1a0
Kanghee <kh63167574@gmail.com>
2026-08-19 00:44:59 +0900

    refactor : 네이밍 변경

--- src/main/java/com/example/devs/domain/user/domain/PersonalHistory.java
+++ src/main/java/com/example/devs/domain/user/domain/PersonalHistory.java
@@ -1,7 +1,7 @@
 package com.example.devs.domain.user.domain;
 
 public enum PersonalHistory {
-    NONE, // 경력없음
+    NO_EXPERIENCE, // 경력없음
     ENTRY_LEVEL, // 신입
     JUNIOR,      // 주니어
     MIDDLE,      // 미들
